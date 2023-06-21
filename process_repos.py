5f565203a3408a68263c2e692a066df2577c9bca
布客飞龙 <562826179@qq.com>
2023-06-21 16:01:02 +0800

    Add files via upload

---
+++ process_repos.py
@@ -0,0 +1,80 @@
+from EpubCrawler.util import request_retry
+import json
+from os  import path
+import base64
+import os
+import hashlib
+
+GHP_TOKEN = os.environ.get('GHP_TOKEN', '')
+headers = {
+    'Authorization': f'Bearer {GHP_TOKEN}'
+}
+
+def get_blob_sha(cont):
+    cont = cont.encode('utf8')
+    cont = b'blob %d\0' % len(cont) + cont
+    return hashlib.sha1(cont).hexdigest()
+
+def list_dir(ns, repo, path):
+    url = f'https://api.github.com/repos/{ns}/{repo}/contents/{path}'
+    r = request_retry('GET', url, headers=headers)
+    if r.status_code != 200: return []
+    j = r.json()
+    return [it['name'] for it in j]
+    
+def read_file(ns, repo, path):
+    url = f'https://api.github.com/repos/{ns}/{repo}/contents/{path}'
+    r = request_retry('GET', url, headers=headers)
+    if r.status_code != 200: return ''
+    j = r.json()
+    return base64.b64decode(j['content'].encode('utf8')).decode('utf8')
+    
+def list_org_repos(ns):
+    res = []
+    while True:
+        url = f'https://api.github.com/orgs/{ns}/repos'
+        r = request_retry('GET', url, headers=headers)
+        if r.status_code != 200: break
+        j = r.json()
+        if len(j) == 0: break
+        res += [it['name'] for it in j]
+    return res
+    
+def update_file(ns, repo, path, cont, old_sha):
+    url = f'https://api.github.com/repos/{ns}/{repo}/contents/{path}'
+    data = {
+        'message': f'update {path}',
+        'content': base64.b64encode(cont.encode('utf8')).decode('utf8'),
+        'sha': old_sha,
+    }
+    r = request_retry('PUT', url, json=data, headers=headers)
+    if r.status_code // 100 == 2:
+        return {'code': 0, 'msg': ''}
+    else:
+        return {'code': r.status_code, 'msg': r.text}
+
+def main():
+    repos_fname = 'repos.json'
+    if path.isfile(repos_fname):
+        repos = json.loads(open(repos_fname, encoding='utf8').read())
+    else:
+        repos = list_org_repos('opendoccn')
+        open(repos_fname, 'w', encoding='utf8').write(json.dumps(repos))
+        
+    for repo in repos:
+        print(repo)
+        rt = list_dir('opendoccn', repo, '')
+        if 'CNAME' not in rt: continue
+        cont = read_file('opendoccn', repo, 'CNAME')
+        if 'apachecn.org' not in cont: continue
+        new_cont = cont.replace('apachecn.org', 'flygon.net')
+        r = update_file(
+            'opendoccn', repo, 'CNAME', new_cont, get_blob_sha(cont))
+        if r['code'] == 0:
+            print(f'{repo} 修改成功')
+        else:
+            msg = r['msg']
+            print(f'{repo} 修改失败：{msg}')
+            
+if __name__ == '__main__': main()
+            
