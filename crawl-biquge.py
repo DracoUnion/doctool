17443d4a5f38494454dfc18750661d303c142618
布客飞龙 <562826179@qq.com>
2023-07-09 19:15:04 +0800

    Add files via upload

--- crawl-biquge.py
+++ crawl-biquge.py
@@ -4,15 +4,25 @@ from os import path
 import os
 from pyquery import PyQuery as pq
 import sys
+import argparse
 
 hdrs = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
 }
 
-st = int(sys.argv[1]) if len(sys.argv) > 1 else 1
-ed = int(sys.argv[2]) if len(sys.argv) > 2 else 1_000_000
+parser = argparse.ArgumentParser(prog="crawl-biquge", formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument("-v", "--version", action="version", version=f"PYBP version: 0.0.0")
+parser.add_argument("-s", "--start", type=int, default=1, help="starting id")
+parser.add_argument("-e", "--end", type=int, default=1_000_000, help="ending id")
+parser.add_argument("-t", "--threads", type=int, default=1, help="thread count")
+parser.add_argument("-p", "--proxy", help="proxy url")
+    
+args = parser.parse_args()
+if args.proxy:
+    args.proxy = {'http': args.proxy, 'https': args.proxy}
+st, ed = args.start, args.end
 
-for i in range(st, ed):
+for i in range(st, ed + 1):
     url = f'http://www.biqu520.net/0_{i}/'
     while True:
         r = requests.get(url, headers=hdrs)
@@ -36,9 +46,10 @@ for i in range(st, ed):
         "link": "#list dt:nth-of-type(2) ~ dd a",
         "title": ".bookname>h1",
         "content": "#content",
-        "textThreads": 24,
+        "textThreads": args.threads,
         "retry": 1000,
         "encoding": "gbk",
+        "proxy": args.proxy,
         "optiMode": "thres",
     }
     open('config_biquge.json', 'w').write(json.dumps(cfg))
