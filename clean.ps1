8a8ba9ba48692bbfc5d67d42fc01f9e1e8407316
布客飞龙 <562826179@qq.com>
2023-10-10 18:35:34 +0800

    Add files via upload

---
+++ clean.ps1
@@ -0,0 +1,15 @@
+mv *.md D:\docs\love-course-2016-2019\docs\an-xiao-yao
+$pwd = $(pwd).ToString()
+cd D:\docs\love-course-2016-2019\
+./update.sh
+cd $pwd
+
+$li = ls -n D:\docs\love-course-2016-2019\docs\an-xiao-yao
+
+$exts = @('mp4', 'mp3', 'avi', 'mkv', 'mov', 'f4v', 'm4a', 'mpeg', 'flv', 'wav', 'wma', 'aac')
+
+foreach($f in $li){
+foreach($e in $exts){
+rm $f.replace('.md', '.' + $e)
+}
+}
