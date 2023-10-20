bbf4ba8e290b502522cbd04c508d0dd181064bd7
布客飞龙 <562826179@qq.com>
2023-10-20 18:18:31 +0800

    Add files via upload

--- clean.ps1
+++ clean.ps1
@@ -2,7 +2,7 @@
 function mv_md_rm_aud($aud_dir, $md_dir){
 	mv "$aud_dir\*.md" $md_dir
 	$pwd = $(pwd).ToString()
-	cd $md_dir
+	cd $md_dir\..\..
 	git pull
 	./update.sh
 	cd $pwd
@@ -17,5 +17,8 @@ function mv_md_rm_aud($aud_dir, $md_dir){
 	}
 }
 
-mv_md_rm_aud D:\bili\19-5-moka  D:\docs\love-course-2016-2019\docs\mo-ka
-mv_md_rm_aud D:\bili\19-8-wuya  D:\docs\love-course-2016-2019\docs\wu-ya
+for(;;){
+	mv_md_rm_aud D:\bili\19-5-moka  D:\docs\love-course-2016-2019\docs\mo-ka
+	mv_md_rm_aud D:\bili\19-8-wuya  D:\docs\love-course-2016-2019\docs\wu-ya
+	sleep 600
+}
