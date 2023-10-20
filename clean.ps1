41b082e0a3db56871d030dfe7810085409c06c6c
布客飞龙 <562826179@qq.com>
2023-10-20 18:13:19 +0800

    Add files via upload

--- clean.ps1
+++ clean.ps1
@@ -1,17 +1,21 @@
-$d = 'D:\docs\love-course-2016-2019\docs\an-xiao-yao'
-mv *.md $d
-$pwd = $(pwd).ToString()
-cd $d\..\..
-git pull
-./update.sh
-cd $pwd
 
-$li = ls -n $d
+function mv_md_rm_aud($aud_dir, $md_dir){
+	mv "$aud_dir\*.md" $md_dir
+	$pwd = $(pwd).ToString()
+	cd $md_dir
+	git pull
+	./update.sh
+	cd $pwd
+	$exts = @('mp4', 'mp3', 'avi', 'mkv', 'mov', 'f4v', 'm4a', 'mpeg', 'flv', 'wav', 'wma', 'aac')
 
-$exts = @('mp4', 'mp3', 'avi', 'mkv', 'mov', 'f4v', 'm4a', 'mpeg', 'flv', 'wav', 'wma', 'aac')
-
-foreach($f in $li){
-foreach($e in $exts){
-rm $f.replace('.md', '.' + $e)
+	$li = ls -n $md_dir
+	foreach($f in $li){
+		foreach($e in $exts){
+			$nf = $f.replace('.md', '.' + $e)
+			rm $aud_dir\$nf
+		}
+	}
 }
-}
+
+mv_md_rm_aud D:\bili\19-5-moka  D:\docs\love-course-2016-2019\docs\mo-ka
+mv_md_rm_aud D:\bili\19-8-wuya  D:\docs\love-course-2016-2019\docs\wu-ya
