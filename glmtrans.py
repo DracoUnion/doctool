3f32bb56a081904f1b04ac571a0f4f8dcc2d822c
布客飞龙 <562826179@qq.com>
2024-01-08 16:19:42 +0800

    Add files via upload

--- glmtrans.py
+++ glmtrans.py
@@ -95,6 +95,7 @@ def train_handle(args):
     llm, tokenizer = load_pytorch_llm(
         args.base_path, args.model_path, args.lora_path)
     llm.attach_lora()
+    torch.autograd.set_detect_anomaly(True)
     optimizer = torch.optim.SGD(llm.parameters(), lr=args.lr)
     step = 0
     for epoch in range(args.n_epoch):
@@ -116,7 +117,8 @@ def train_handle(args):
             input_ids = torch.tensor([input_ids]).cuda()
             output_ids = torch.tensor([output_ids]).cuda()
             loss = llm.forward(input_ids=input_ids, labels=output_ids, return_dict=True).loss
-            loss.backward()
+            with torch.autograd.detect_anomaly(): 
+                loss.backward()
             print(
                 f'epoch: {epoch}\n' + 
                 f'step: {step}\n' + 
@@ -124,16 +126,16 @@ def train_handle(args):
                 f'ans: {json.dumps(ans, ensure_ascii=False)}\n' + 
                 f'loss: {loss}'
             )
-            # 一定步骤再更新梯度
-            if step % args.grad_accum == 0:
-                optimizer.step()
+            # 更新梯度
+            torch.nn.util.clip_(llm.parameters(), 0.1)
+            optimizer.step()
+            torch.cuda.empty_cache()
+            torch.cuda.ipc_collect()
             # 一定步骤保存权重
             if step % args.save_step == 0:
                 torch.save(llm.lora_state_dict(), args.save_path)
             step += 1
 
-    if step % args.grad_accum != 0:
-        optimizer.step()
     if step % args.save_step != 0:
         torch.save(llm.lora_state_dict(), args.save_path)
      
@@ -157,7 +159,6 @@ def main():
     train_parser.add_argument("-b", "--base-path", default='/data/chatglm2-6b-int4-lora/model', help="path for model code")
     train_parser.add_argument("-m", "--model-path", help="path for model param (optional)")
     train_parser.add_argument("-l", "--lora-path", help="path for lora param")
-    train_parser.add_argument("--grad-accum", type=int, default=1, help="grad_accum")
     train_parser.add_argument("--lr", type=float, default=5e-2, help="lr")
     train_parser.add_argument("--save-step", type=int, default=30, help="save_step")
     train_parser.add_argument("-n", "--n-epoch", type=int, default=15, help="n_epoch")
