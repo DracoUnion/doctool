3c48f7621e12a27647fa62bc0969808f5d1cd32c
布客飞龙 <562826179@qq.com>
2024-03-26 15:44:10 +0800

    Add files via upload

--- soepub2md.py
+++ soepub2md.py
@@ -2,6 +2,9 @@ from BookerEpubTool.util import *
 from BookerMarkdownTool.tomd import tomd
 import argparse
 from pyquery import PyQuery as pq
+# from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+import time
 
 def get_title(html):
     if isinstance(html, bytes):
@@ -10,6 +13,16 @@ def get_title(html):
     title = (pq(html).find('h1').eq(0).html() or '').strip()
     print(title)
     return title
+
+def tr_write_md(html, dir):
+    md = tomd(html)
+    m = re.search(r'ID：(\d+)', md)
+    if not m: return
+    id_ = m.group(1).zfill(9)
+    print(id_)
+    ofname = path.join(dir, f'{id_}.md')
+    open(ofname, 'w', encoding='utf8').write(md)
+    time.sleep(0)
 
 def epub2md(args):
     if not args.fname.endswith('.epub'):
@@ -23,19 +36,19 @@ def epub2md(args):
            get_title(cont) != '-'
     ]
 
+    pool = ProcessPoolExecutor(args.threads)
+    hdls = []
     for html in htmls:
-        md = tomd(html)
-        m = re.search(r'ID：(\d+)', md)
-        if not m: continue
-        id_ = m.group(1).zfill(9)
-        print(id_)
-        ofname = path.join(args.dir, f'{id_}.md')
-        open(ofname, 'w', encoding='utf8').write(md)
+        hdl = pool.submit(tr_write_md, html, args.dir)
+        hdls.append(hdl)
+    for h in hdls: h.result()
+    
 
 def main():
     parser = argparse.ArgumentParser('soepub2md')
     parser.add_argument('fname', help='epub fname')
     parser.add_argument('-d', '--dir', default='.', help='output dir')
+    parser.add_argument('-t', '--threads', type=int, default=16, help='thread num')
     args = parser.parse_args()
     epub2md(args)
 
