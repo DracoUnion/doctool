2f9030a12dff29449933bfcae754d1c9fd83033f
布客飞龙 <562826179@qq.com>
2024-09-30 19:21:20 +0800

    Add files via upload

--- dlashare.py
+++ dlashare.py
@@ -2,8 +2,10 @@ import os
 import sys
 from os import path
 from Ashare import *
+from concurrent.futures import ThreadPoolExecutor
+import argparse
 
-def dl_single(code, retry):
+def get_single(code, retry):
     for i in range(retry):
         try:
             return  get_price(
@@ -15,19 +17,37 @@ def dl_single(code, retry):
             print(f'{code} retry #{i+1}: {ex}')
             if i == retry - 1: raise ex
         
+def tr_dl_single(code, retry):
+    print(code)
+    fname = f'{code}_20201231_day.csv'      
+    if path.isfile(fname):
+        print(f'{code} 已存在')
+        return
+    df = get_single(code, retry)
+    df.to_csv(fname)
+
+        
 def main():
-    st = int(sys.argv[1]) if len(sys.argv) > 1 else 1
-    ed = int(sys.argv[2]) if len(sys.argv) > 2 else 99999
-    retry = int(sys.argv[3]) if len(sys.argv) > 3 else 1_000_000_000
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--start', type=int, default=1)
+    parser.add_argument('-e', '--end', type=int, default=99999)
+    parser.add_argument('-r', '--retry', type=int, default=1_000_000_000)
+    parser.add_argument('-t', '--threads', type=int, default=8)
+    args = parser.parse_args()
+
+    st, ed, retry, threads = args.start, args.end, args.retry, args.threads
     
+    pool = ThreadPoolExecutor(threads)
+    hdls = []
     for i in range(st, ed + 1): 
         code = f'6{i:05d}.XSHG'
-        print(code)
-        fname = f'{code}_20201231_day.csv'      
-        if path.isfile(fname):
-            print(f'{code} 已存在')
-            continue
-        df = dl_single(code, retry)
-        df.to_csv(fname)
+        h = pool.submit(tr_dl_single, code , retry)
+        hdls.append(h)
+        if len(hdls ) >= threads:
+            for h in hdls: h.result()
+            hdls.clear()
+    
+    for h in hdls: h.result()
+
         
 if __name__ == '__main__': main()
