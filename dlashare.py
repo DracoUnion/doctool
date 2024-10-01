6e4716ee0025c7c37eab449a6c79bb6f5f94c5a6
布客飞龙 <562826179@qq.com>
2024-10-01 08:31:39 +0800

    Add files via upload

--- dlashare.py
+++ dlashare.py
@@ -5,11 +5,11 @@ from Ashare import *
 from concurrent.futures import ThreadPoolExecutor
 import argparse
 
-def get_single(code, retry):
+def get_single(code, retry, freq):
     for i in range(retry):
         try:
             return  get_price(
-                code, frequency='1d', 
+                code, frequency=f'1{freq}', 
                 count=1_000_000_000,
                 end_date='2020-12-31'
             )
@@ -17,22 +17,24 @@ def get_single(code, retry):
             print(f'{code} retry #{i+1}: {ex}')
             if i == retry - 1: raise ex
         
-def tr_dl_single(code, retry):
+def tr_dl_single(code, retry, freq):
     print(code)
-    fname = f'{code}_20201231_day.csv'      
+    freq_txt = 'day' if freq == 'd' else 'min'
+    fname = f'{code}_20201231_{freq_txt}.csv'      
     if path.isfile(fname):
         print(f'{code} 已存在')
         return
-    df = get_single(code, retry)
+    df = get_single(code, retry, freq)
     df.to_csv(fname)
 
         
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--start', type=int, default=1)
-    parser.add_argument('-e', '--end', type=int, default=99999)
+    parser.add_argument('-e', '--end', type=int, default=3999)
     parser.add_argument('-r', '--retry', type=int, default=1_000_000_000)
     parser.add_argument('-t', '--threads', type=int, default=8)
+    parser.add_argument('-f', '--freq', choices=['m', 'd'], default='d')
     args = parser.parse_args()
 
     st, ed, retry, threads = args.start, args.end, args.retry, args.threads
@@ -41,7 +43,7 @@ def main():
     hdls = []
     for i in range(st, ed + 1): 
         code = f'6{i:05d}.XSHG'
-        h = pool.submit(tr_dl_single, code , retry)
+        h = pool.submit(tr_dl_single, code , retry, args.freq)
         hdls.append(h)
         if len(hdls ) >= threads:
             for h in hdls: h.result()
