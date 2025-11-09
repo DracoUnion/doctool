83329589b6771c161403a33157a082b07373921f
布客飞龙 <562826179@qq.com>
2025-11-09 19:37:57 +0800

    Add files via upload

--- gen_crawler_list.py
+++ gen_crawler_list.py
@@ -1,14 +1,19 @@
-
+import json
 import requests
 from pyquery import PyQuery as pq
 from urllib.parse import urljoin
+import subprocess as subp
 
 config = {
-    'name': 'runoob',
+    'name': '菜鸟教程（runoob）',
     'url': 'https://www.runoob.com/',
     'doc': '.codelist>h2, .codelist>a',
     'chapter': '#leftcolumn>h2, #leftcolumn>a',
-    
+    'docName': 'body > div.container.main > div > div.runoob-col-md2 > div > div.tab > span',
+	"title": "#content>h1",
+	"content": "#content",
+	"remove": "script, ins, iframe",
+	"optiMode": "quant",
 
 }
 
@@ -20,27 +25,32 @@ def main():
     for el in el_docs:
         el = pq(el)
         if not el.attr('href'):
+            '''
             txt = el.text().strip()
             print(txt)
             ofile.write(txt + '\n')
+            '''
             continue
         url = el.attr('href')
         url = urljoin(config['url'], url)
         print(url)
         html = requests.get(url).text
         doc_rt = pq(html)
-        el_chs =  doc_rt.find(config['chapter'])
-        for elsub in el_chs:
-            elsub = pq(elsub)
-            if not elsub.attr('href'):
-                txt = elsub.text().strip()
-                print(txt)
-                ofile.write(txt + '\n')
-            else:
-                suburl = elsub.attr('href')
-                suburl = urljoin(url, suburl)
-                print(suburl)
-                ofile.write(suburl + '\n')
+        doc_name = doc_rt.find(config['docName']).text().strip()
+
+        cralwer_cfg = {
+            'name': config['name'] + doc_name,
+            'url': url,
+            'title': config['title'],
+            'content': config['content'],
+            'remove': config['remove'],
+            'optiMode': config['optiMode'],
+            'link': config['chapter'],
+            'textThreads': 4,
+            'imgThreads': 4,
+        }
+        open('config_.json', 'w', encoding='utf8').write(json.dumps(cralwer_cfg))
+        subp.run(['crawl-epub', 'config_.json'])
                 
     print('done...')
     
