132a89dc9c0b961ac2c9a37e621d7a7cc4c65315
布客飞龙 <562826179@qq.com>
2025-12-16 18:54:58 +0800

    Add files via upload

--- yymanhua.py
+++ yymanhua.py
@@ -2,6 +2,8 @@ import os
 from os import path
 import shutil
 import requests
+import numpy as np
+import cv2
 from pyquery import PyQuery as pq
 from imgyaso import pngquant_bts, \
     adathres_bts, grid_bts, noise_bts, trunc_bts, noisebw_bts
@@ -17,11 +19,27 @@ import subprocess as subp
 from concurrent.futures import ThreadPoolExecutor
 from BookerDownloadTool.util import *
 import argparse
+from img2pdf import convert as img2pdf
 
 ch_pool = None
 img_pool = None
 exi_list = set()
     
+def highlight(img):
+    is_bytes = isinstance(img, bytes)
+    if is_bytes:
+        img = np.frombuffer(img, np.uint8)
+        img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
+    img[img >= 250] = 255
+    img[img <= 5] = 0
+    if is_bytes:
+        img = cv2.imencode(
+            '.png', img, 
+            [cv2.IMWRITE_PNG_BILEVEL, 1]
+        )[1]
+        img = bytes(img)
+    return img
+
 def load_exi_list(args):
     global exi_list
     if not exi_list and args.exi_list and path.exists(args.exi_list):
@@ -51,7 +69,7 @@ def get_article(html):
     dt = re.search('YYMANHUA_VIEWSIGN_DT="(.+?)"', html).group(1)
     total = re.search('YYMANHUA_IMAGE_COUNT=(\d+)', html).group(1)
     total = int(total)
-    
+
     hdrs = default_hdrs | {
         'Referer': 'https://yymanhua.com/',
     }
@@ -113,7 +131,8 @@ def download_dmzj_ch(url, info, odir):
         imgs.get(f'{i}.png', b'')
         for i in range(len(imgs))
     ]
-    pdf = img2jb2pdf(img_list)
+    # pdf = img2jb2pdf(img_list)
+    pdf = img2pdf(img_list)
     open(ofname, 'wb').write(pdf)
     
 def init_pools(args):
@@ -176,10 +195,14 @@ def fetch_dmzj(args):
     while True:
         if stop: break
         print(f'page: {i}')
-        url = f'http://sacg.idmzj.com/mh/index.php?c=category&m=doSearch&status=0&reader_group=0&zone=2304&initial=all&type=0&_order=t&p={i}&callback=c'
-        res = request_retry('GET', url, headers=dmzj_hdrs).text
-        j = json.loads(res[2:-2])
-        if not j.get('result'): break
+        url = f'https://yymanhua.com/manga-list-0-0-2-p{i}/'
+        res = request_retry('GET', url, headers=default_hdrs).text
+        # j = json.loads(res[2:-2])
+        rt = pq(res)
+        el_links = rt('h2.title>a')
+        ids = [pq(el).attr('href').replace('/', '') for el in el_links]
+        if not ids: break
+        '''
         for bk in j['result']:
             id = bk['comic_url'][1:-1]
             dt = bk['last_update_date'].replace('-', '')
@@ -190,18 +213,42 @@ def fetch_dmzj(args):
                 break
             print(id, dt)
             f.write(id + '\n')
+        '''
+        for id in ids:
+            print(id)
+            f.write(id + '\n')
         i += 1
         
     f.close()
         
 def main():
-    id = sys.argv[1]
-    download_dmzj(argparse.Namespace(
-        id=id, 
-        ch_threads=4,  
-        img_threads=4,
-        exi_list=None,
-        out='.',
-    ))
+    parser = argparse.ArgumentParser(prog="BookerPubTool", formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.set_defaults(func=lambda x: parser.print_help())
+    subparsers = parser.add_subparsers()
+    dl_parser = subparsers.add_parser("download")
+    dl_parser.add_argument("id")
+    dl_parser.add_argument("-ct", '--ch-threads', type=int, default=4)
+    dl_parser.add_argument("-it", '--img-threads', type=int, default=4)
+    dl_parser.add_argument("-l", '--exi-list', type=str, default=None)
+    dl_parser.add_argument("-o", '--out', type=str, default='.')
+    dl_parser.set_defaults(func=download_dmzj)
+
+    fetch_parser = subparsers.add_parser("fetch")
+    fetch_parser.add_argument("fname")
+    fetch_parser.add_argument("-s", '--start', type=int, default=0)
+    fetch_parser.add_argument("-e", '--end', type=int, default=1_000_000)
+    fetch_parser.set_defaults(func=fetch_dmzj)
+
+    batch_parser = subparsers.add_parser("batch")
+    batch_parser.add_argument("fname")
+    batch_parser.add_argument("-ct", '--ch-threads', type=int, default=4)
+    batch_parser.add_argument("-it", '--img-threads', type=int, default=4)
+    batch_parser.add_argument("-l", '--exi-list', type=str, default=None)
+    batch_parser.add_argument("-o", '--out', type=str, default='.')
+    batch_parser.set_defaults(func=batch_dmzj)
+
+    args = parser.parse_args()
+    args.func(args)
+
 
 if __name__ == '__main__': main()
